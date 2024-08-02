d464953eaa1dbf34a8139706d16fa71406473ea4
AbhijithCSundaran <161324727+AbhijithCSundaran@users.noreply.github.com>
2024-08-02 18:50:22 +0530

    updated

--- JetBlue_Credit_NoFee/r2/base/JetBlue_Credit_NoFee_Route2_300x250_Base/manifest.js
+++ JetBlue_Credit_NoFee/r2/base/JetBlue_Credit_NoFee_Route2_300x250_Base/manifest.js
@@ -1,14 +1,14 @@
 FT.manifest({
-    "filename": "index.html",
-    "width": 300,
-    "height": 250,
-    "clickTagCount": 1,
-    "hideBrowsers": ["ie8"],
-    "richloads": [
- 		{"name":"main_rl", "src":"JetBlue_Credit_NoFee_Route2_300x250_RichLoad"}
- 	],
-    "instantAds": [
-        {"name":"main_rl",          "type":"richload"}, 
-        {"name":"clickTag1_url",    "type":"text", "default":"http://www.landingpageurl.com/?blahblah"}
-    ]
+	"filename": "index.html",
+	"width": 300,
+	"height": 250,
+	"clickTagCount": 3,
+	"hideBrowsers": ["ie8"],
+	"richLoads": [
+		{ "name": "RL1", "src": "JetBlue_Credit_NoFee_Route2_300x250_RichLoad" },
+	],
+	"instantAds": [
+		{ "name": "RL1", "type": "richLoad" },
+		{ "name": "clickTag1_url", "type": "text", "default": "http://www.landingpageurl.com/?blahblah" }
+	]
 });
